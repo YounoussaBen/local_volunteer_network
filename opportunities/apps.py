a1c00e938175b39fd3d522e739125212c1fc9b56
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-04-01 09:10:00 +0000

    Create opportunities application

---
+++ opportunities/apps.py
@@ -0,0 +1,10 @@
+# opportunities/apps.py
+
+from django.apps import AppConfig
+
+class OpportunitiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'opportunities'
+    
+    def ready(self):
+        import opportunities.signals
